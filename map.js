964b4011e8ce9c5d9483fffb142e06a34f52b1b1
Dinesh Patra <dineshpatra28@gmail.com>
2016-08-17 20:27:52 +0530

    from address

--- map.js
+++ map.js
@@ -1,6 +1,7 @@
 (function($) {
     var map = null;
     var originMarkerArr = [];
+    var origin = {};
 
     $(document).ready(function() {
         var geocoder = new google.maps.Geocoder();
@@ -19,7 +20,7 @@
         var destinationIcon = 'https://chart.googleapis.com/chart?' +
             'chst=d_map_pin_letter&chld=D|FF0000|000000';
 
-        $("#from-address").on('focus', function() {
+        $("#from-address, #to-address").on('focus', function() {
              $(this).parent().removeClass('has-error');
          });
         $("#from-address").on('blur', function() {
@@ -32,6 +33,9 @@
             geocoder.geocode({'address': address}, function(results, status) {
                 if (status === 'OK') {
                     map.fitBounds(bounds.extend(results[0].geometry.location));
+                    origin['lat'] = results[0].geometry.location.lat();
+                    origin['lng'] = results[0].geometry.location.lng();
+                    origin['addr'] = results[0].formatted_address; 
                     originMarkerArr.push(new google.maps.Marker({
                         map: map,
                         position: results[0].geometry.location,
@@ -44,6 +48,71 @@
             });
 
         });
+        var destMarkerArr = [];
+        var dest = {};
+        $("#to-address").on('blur', function() {
+            var address = $(this).val();
+            deleteDestMarkers();
+            if (!address) {
+                $("#to-address").parent().addClass('has-error');
+                return;
+            }
+            geocoder.geocode({'address': address}, function(results, status) {
+                if (status === 'OK') {
+                    map.fitBounds(bounds.extend(results[0].geometry.location));
+                    dest['lat'] = results[0].geometry.location.lat();
+                    dest['lng'] = results[0].geometry.location.lng();
+                    dest['addr'] = results[0].formatted_address; 
+                    destMarkerArr.push(new google.maps.Marker({
+                        map: map,
+                        position: results[0].geometry.location,
+                        icon: destinationIcon
+                    }));
+                    $("#to-address").parent().removeClass('has-error').addClass('has-success');
+                } else {
+                    $("#to-address").parent().addClass('has-error');
+                }
+            });
+
+        });
+    
+        var service = new google.maps.DistanceMatrixService;
+        $("#twitter-post").on('click', function(e) {
+            distance = 0;
+            orLat = origin.lat;
+            orLng = origin.lng;
+            orAddr = origin.addr; 
+            originArr = [{lat: orLat, lng:orLng}, orAddr];
+
+            deLat = dest.lat;
+            deLng = dest.lng;
+            deAddr = dest.addr;
+            destArr = [{lat: deLat, lng:deLng}, deAddr];
+
+            service.getDistanceMatrix({
+                origins: originArr,
+                destinations: destArr,
+                travelMode: 'DRIVING',
+                unitSystem: google.maps.UnitSystem.METRIC,
+                avoidHighways: false,
+                avoidTolls: false
+            }, function(response, status) {
+                if (status == 'OK') { 
+                    for (var i in response.rows) {
+                        distance = response.rows[i].elements[0].distance.value; 
+                    }
+                    distance = distance/1000;
+                    alert(distance);
+                }
+            });
+        });
+
+        function deleteDestMarkers() {
+            for (var i = 0; i < destMarkerArr.length; i++) {
+                destMarkerArr[i].setMap(null);
+            }
+            destMarkerArr = [];
+        }
 
 
         function deleteOriginMarkers() {
